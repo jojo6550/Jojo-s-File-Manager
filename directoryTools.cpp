a66add1e4d15a372134decf6cc39fde6dc3be775
jojo6550 <josiah.johnson6550@gmail.com>
2025-04-06 21:28:11 -0500

    Can create,edit delete, move into and list all directories.

--- directoryTools.cpp
+++ directoryTools.cpp
@@ -4,11 +4,11 @@
 
 namespace fs = std::filesystem;
 
-class directoryTools
-{
+class directoryTools{
 private:
     //define root directory
     fs::path root = "root";
+    fs::path currentDirectory = root;
 
 public:
     directoryTools(fs::path root){
@@ -18,18 +18,71 @@ public:
             fs::create_directory(root);
         }
     }
+    //returns current directory inside root
+    fs::path getCurrentDirectory(){
+        return currentDirectory;
+    }
 
-    void createSubdirectory(fs::path subDirectoryPath){
-        if(fs::exists(root / subDirectoryPath) || fs::is_directory(root / subDirectoryPath)){
-            std::cout << subDirectoryPath << " already exists!" << std::endl;
+    void returnToRoot(){
+        currentDirectory = root;
+    }
+
+    void changeDirectory(fs::path subDirectoryPath){
+        fs::path newDirectory = currentDirectory / subDirectoryPath;
+
+        if(fs::exists(newDirectory)){
+            currentDirectory = newDirectory;
         } else {
-            fs::create_directory(root / subDirectoryPath);
+            std::cout << "Directory does not exist." << std::endl;
         }
     }
 
-    void renameSubdirectory(fs::path oldDirectoryName, fs::path newDirectoryName){
-        fs::rename(oldDirectoryName,newDirectoryName);
+    //creates directory
+    void createSubdirectory(fs::path subDirectoryPath){
+        if(fs::exists(getCurrentDirectory() / subDirectoryPath) || fs::is_directory(getCurrentDirectory() / subDirectoryPath)){
+            std::cout << subDirectoryPath << " already exists!" << std::endl;
+        } else {
+            fs::create_directory(getCurrentDirectory() / subDirectoryPath);
+        }
     }
+    //renames directory
+    void renameSubdirectory(fs::path oldDirectoryName, fs::path newDirectoryName){
+        if(!fs::exists(getCurrentDirectory()/ oldDirectoryName) || !fs::is_directory(getCurrentDirectory() / oldDirectoryName)){
+            std::cout << "Directory does not exist..." << std::endl;
+        } else {
+            fs::rename(getCurrentDirectory() / oldDirectoryName, getCurrentDirectory() / newDirectoryName);
+        }
+    }
+    //deletes directory
+    void deleteSubdirectory(fs::path subDirectoryPath){
+        if(!fs::exists(getCurrentDirectory() / subDirectoryPath) || !fs::is_directory(getCurrentDirectory() / subDirectoryPath)){
+            std::cout << "Directory does not exist..." << std::endl;
+        } 
+
+        std::cout << "Please enter directory name to confirm deletion order: " << std::endl;
+        
+        std::string dirName;
+
+        std::cin >> dirName;
+
+        if(dirName == subDirectoryPath.string()){
+            fs::remove( getCurrentDirectory() / subDirectoryPath);
+        } else {
+            std::cout << "Deletion order cancelled..." << std::endl;
+        }
+    }
+    
+
+    void listSubdirectories(){
+        fs::path workingDir = getCurrentDirectory();
+        std::cout << "Current directory: " << workingDir << std::endl;
+        for(const auto var : fs::directory_iterator(workingDir)){
+            std::cout << var << std::endl;
+        }
+
+    }
+
+    
 
 };
 
